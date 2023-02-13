a57220f651ba7463e9c0dcc396fa0095e944b9ed
SincereTuitt <110364655+SincereTuitt@users.noreply.github.com>
2023-02-13 15:23:07 -0500

    Add files via upload

---
+++ site.js
@@ -0,0 +1,4 @@
+function notifySincere () {
+    window.location.href = "mailto:KayleenNeedsAttention@gmail.com?subject=Give Kayleen Attention!&body=Do it now";
+    alert("Sincere has been notified")
+}
